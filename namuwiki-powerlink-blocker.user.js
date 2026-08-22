4e518be2f589b18efe940435438df5078435c2f9
fifoqueue <me@hsr.vg>
2026-08-22 20:02:38 +0900

    fix: version bump for fix

--- namuwiki-powerlink-blocker.user.js
+++ namuwiki-powerlink-blocker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NamuWiki PowerLink Blocker
 // @namespace    List-KR
-// @version      2.9.0
+// @version      2.9.2
 // @description  Block NamuWiki PowerLink
 // @match        https://namu.wiki/*
 // @updateURL    https://raw.githubusercontent.com/List-KR/namuwiki-powerlink-blocker/refs/heads/main/namuwiki-powerlink-blocker.user.js
